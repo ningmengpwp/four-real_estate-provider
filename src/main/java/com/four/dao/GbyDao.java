49adebd6916de25d20ab8c41779ab42dc36325aa
guobaoyu <1751845992@qq.com>
2018-06-01 19:01:53 +0800

    dsfs

--- src/main/java/com/four/dao/GbyDao.java
+++ src/main/java/com/four/dao/GbyDao.java
@@ -1,7 +1,6 @@
 package com.four.dao;
 
-import com.four.entity.Apartment;
-import com.four.entity.Feature;
+import com.four.entity.*;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
@@ -40,6 +39,78 @@ public interface GbyDao {
     Feature queryByIdwuyetese(Integer id);
     @Update("update t_feature set name=#{name},sell=#{sell},rendout=#{rendout},programa=#{programa},xuhao=#{xuhao} where id=#{id}")
     void updatewuyetese(Feature feature);
-    @Select("select * from huiji h")
-    List<Map<String,Object>> queryjingji();
+    @Select("select * from huiji a,huijin b,huizcxz c where a.huijinid=b.huijinid and a.huijizhucexz=c.huizcxzid  and c.huizcxzid=2 ")
+    List<Map<Object,String>> queryjingji();
+    @Select("select * from huiji a,huijin b,huizcxz c where a.huijinid=b.huijinid and a.huijizhucexz=c.huizcxzid and a.huijiid=#{id}")
+    Huiji updatesyhyhuicha(@Param("id")String id);
+    @Select("select * from huideng")
+    List<Huideng> queryhydj();
+    @Select("select * from t_area where pid=0")
+    List<Area> querydrea();
+    @Select("select * from t_area where pid=#{id}")
+    List<Area> querydreadifangid(String id);
+    @Update("update huiji set  huijipass=#{huijipass}, huijilianxiren=#{huijilianxiren}, huijiyouxiang=#{huijiyouxiang}, huijinicheng=#{huijinicheng}, huijishifoubyx=#{huijishifoubyx}, huijishifoubsj=#{huijishifoubsj}, huijishouji=#{huijishouji}, huijidianhua=#{huijidianhua}, huijiqq=#{huijiqq}, huijiquyu=#{huijiquyu}, huijiquyudf=#{huijiquyudf}, huijitouxiang=#{huijitouxiang},huijierweima=#{huijierweima}, huijimendiandz=#{huijimendiandz}, huijizhongjiemc=#{huijizhongjiemc}, huijizwojsgc=#{huijizwojsgc}, huijidituzb=#{huijidituzb}, huijidpgjc=#{huijidpgjc}, huijinid=#{huijinid}, huisysxl=#{huisysxl}, huiquanxian=#{huiquanxian}, huidjdqdate=#{huidjdqdate},huidengid=#{huidengid} where huijiid=#{huijiid}")
+    void updatesyhygai(Huiji hj);
+    @Select("<script>" +
+            "select  * from t_chuzugl c,t_apartment a,  t_fysh f, t_storetype s " +
+            "<where>" +
+            " c.chuzuid=a.id and " +
+            "c.chuzushzt=f.fyshztid and " +
+            "c.shangpulxid=s.id "+
+            "<if test=' wuyeid !=null || \"\".equals(wuyeid) '>" +
+            "and c.wuyeid=#{wuyeid}" +
+            "</if>" +
+            "<if test='chuzuname !=null || \"\".equals(chuzuname) '>" +
+            "and c.chuzuname like '%${chuzuname}%'" +
+            "</if>" +
+            "</where>" +
+            "</script>")
+    List<Map<String,Object>> querychuzu(@Param("wuyeid") Integer wuyeid,@Param("chuzuname") String chuzuname);
+    @Select("SELECT * from t_chuzugl c ,t_storetype s WHERE c.chuzuid=s.id and c.chuzuid=#{id}")
+    List<Map<String,Object>> querychuzuxx(Integer id);
+    @Select("select * from t_public ")
+    List<Public> querygongjiao();
+    @Select("select * from t_metro ")
+    List<Metro> queryditie();
+    @Select("select * from t_feature ")
+    List<Feature> querywuye();
+    @Select("select * from t_mating ")
+    List<Mating> querypeitao();
+    @Select("select * from t_retailindustry ")
+    List<Retailindustry> queryhangye();
+    @Select("select * from t_storetype ")
+    List<Storetype> querypumian();
+    @Select("select * from t_pavementtype ")
+    List<Pavenmenttype> querypumian1();
+    @Select("select * from t_situation ")
+    List<Situation> queryfangwu();
+    @Select("select * from t_orientation ")
+    List<Orientation> querychaoxiang();
+    @Select("select * from t_chuzugl where chuzuid=#{id}")
+    t_chuzugl queryByIdchuzu(Integer id);
+    @Update("update t_chuzugl set chuzuname=#{chuzuname},zdybianhao=#{zdybianhao},chuzumianji=#{chuzumianji}," +
+            "chuzuzujin=#{chuzuzujin},chuzuzrf=#{chuzuzrf},chuzufs=#{chuzufs}," +
+            "chuzuszxq=#{chuzuszxq},chuzulouc=#{chuzulouc},chuzuqypid=#{chuzuqypid},chuzuwz=#{chuzuwz}" +
+            ",chuzuxgt=#{chuzuxgt},gongjiaoid=#{gongjiaoid}," +
+            "ditieid=#{ditieid},hangyelxid=#{hangyelxid},wuyefei=#{wuyefei}," +
+            "fwqkid=#{fwqkid},wytsid=#{wytsid},pzssid=#{pzssid}," +
+            "chuzujjdm=#{chuzujjdm},zhuzuspdz=#{zhuzuspdz},chuzuxxms=#{chuzuxxms}," +
+            "chuzusxj=#{chuzusxj},chuzucctj=#{chuzucctj},chuzulxr=#{chuzulxr}," +
+            "chuzulxhm=#{chuzulxhm},chuzufbhy=#{chuzufbhy},chuzutjxh=#{chuzutjxh}," +
+            "chuzuzdxh=#{chuzuzdxh},chuzushzt=#{chuzushzt} where chuzuid=#{chuzuid}")
+    void updatechuzu(t_chuzugl chuzugl);
+    @Select("select t.chuzuid,t.chuzusxj from  t_chuzugl t where t.chuzuid in(${id})")
+    List<Map<String,Object>> queryShX(String id);
+    @Update(" update t_chuzugl set  chuzusxj=2  where chuzuid=${idd}")
+    void updateShang(Object chuzuid);
+    @Update(" update t_chuzugl set  chuzusxj=1  where chuzuid =${iddd}")
+    void updateXia(Object chuzuid);
+    @Update("update t_chuzugl c set c.fyshid=${zt} where c.chuzuid in(${ids})")
+    void updateBinG(String ids, Integer zt);
+    @Delete("DELETE FROM t_chuzugl WHERE chuzuid in(${ids})")
+    void deleteschuzu(String ids);
+    @Update(" update t_chuzugl c set c.chuzucctj=0 where c.chuzuid in (${ids})")
+    void updatechutuidw(String ids);
+    @Update(" update t_chuzugl c set c.chuzucctj=1 where c.chuzuid in (${ids})")
+    void updateqxchutuidw(String ids);
 }
--- src/main/java/com/four/service/impl/GbyServiceImpl.java
+++ src/main/java/com/four/service/impl/GbyServiceImpl.java
@@ -1,8 +1,7 @@
 package com.four.service.impl;
 
 import com.four.dao.GbyDao;
-import com.four.entity.Apartment;
-import com.four.entity.Feature;
+import com.four.entity.*;
 import com.four.service.GbyService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -94,9 +93,129 @@ public class GbyServiceImpl implements GbyService{
     }
     //查询经纪人
     @Override
-    public List<Map<String, Object>> queryjingji() {
+    public List<Map<Object, String>> queryjingji() {
         return gbyDao.queryjingji();
     }
+    //查询回显所有会员
+    @Override
+    public Huiji updatesyhyhuicha(String id) {
+        return gbyDao.updatesyhyhuicha(id);
+    }
+    //查询所有会员等级
+    @Override
+    public List<Huideng> queryhydj() {
+        return gbyDao.queryhydj();
+    }
+    //查询所有会员地区
+    @Override
+    public List<Area> querydrea() {
+        return gbyDao.querydrea();
+    }
+    //查询所有会员地区地方
+    @Override
+    public List<Area> querydreadifangid(String id) {
+        return gbyDao.querydreadifangid(id);
+    }
+    //修改所有会员
+    @Override
+    public void updatesyhygai(Huiji hj) {
+        gbyDao.updatesyhygai(hj);
+    }
+   //c出租管理
+    @Override
+    public List<Map<String, Object>> querychuzu(Integer wuyeid, String chuzuname) {
+        return gbyDao.querychuzu(wuyeid,chuzuname);
+    }
 
+    @Override
+    public List<Map<String, Object>> querychuzuxx(Integer id) {
+        return gbyDao.querychuzuxx(id);
+    }
 
+    @Override
+    public List<Public> querygongjiao() {
+        return gbyDao.querygongjiao();
+    }
+
+    @Override
+    public List<Metro> queryditie() {
+        return gbyDao.queryditie();
+    }
+
+    @Override
+    public List<Feature> querywuye() {
+        return gbyDao.querywuye();
+    }
+
+    @Override
+    public List<Mating> querypeitao() {
+        return gbyDao.querypeitao();
+    }
+
+    @Override
+    public List<Retailindustry> queryhangye() {
+        return gbyDao.queryhangye();
+    }
+
+    @Override
+    public List<Storetype> querypumian() {
+        return gbyDao.querypumian();
+    }
+
+    @Override
+    public List<Pavenmenttype> querypumian1() {
+        return gbyDao.querypumian1();
+    }
+
+    @Override
+    public List<Situation> queryfangwu() {
+        return gbyDao.queryfangwu();
+    }
+
+    @Override
+    public List<Orientation> querychaoxiang() {
+        return gbyDao.querychaoxiang();
+    }
+
+    @Override
+    public t_chuzugl queryByIdchuzu(Integer id) {
+        return gbyDao.queryByIdchuzu(id);
+    }
+
+    @Override
+    public void updatechuzu(t_chuzugl chuzugl) {
+        gbyDao.updatechuzu(chuzugl);
+    }
+
+    @Override
+    public void updateShangX(String id) {
+        List<Map<String,Object>> map=gbyDao.queryShX(id);
+        for(int i=0; i<map.size(); i++){
+            if(map.get(i).get("chuzusxj")==Integer.valueOf(1)){
+                gbyDao.updateShang(map.get(i).get("chuzuid"));
+            }else if(map.get(i).get("chuzusxj")==Integer.valueOf(2)){
+                gbyDao.updateXia(map.get(i).get("chuzuid"));
+            }
+        }
+    }
+
+    @Override
+    public void updateBinG(String ids, Integer zt) {
+        gbyDao.updateBinG(ids,zt);
+    }
+
+    @Override
+    public void deleteschuzu(String ids) {
+        gbyDao.deleteschuzu(ids);
+    }
+
+    @Override
+    public void updatechutuidw(String ids) {
+        gbyDao.updatechutuidw(ids);
+    }
+
+    @Override
+    public void updateqxchutuidw(String ids) {
+        gbyDao.updateqxchutuidw(ids);
+    }
 }
